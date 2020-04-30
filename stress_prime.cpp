0c2ee3278b147008e0f113debc24d950c97f58d8
Jack <jcombes9834@gmail.com>
2020-04-30 17:53:11 -0400

    useful commands for benchmarking

--- stress_prime.cpp
+++ stress_prime.cpp
@@ -11,14 +11,14 @@ void primes(int max_seconds, int i);
 
 int main()
 {
-  thread myThread(primes, 10, 2);
-  thread myThread1(primes, 11, 2);
-  thread myThread2(primes, 12, 2);
-  thread myThread3(primes, 13, 2);
-  thread myThread4(primes, 14, 2);
-  thread myThread5(primes, 15, 2);
-  thread myThread6(primes, 16, 2);
-  thread myThread7(primes, 17, 2);
+  thread myThread(primes, 100, 2);
+  thread myThread1(primes, 101, 2);
+  thread myThread2(primes, 102, 2);
+  thread myThread3(primes, 103, 2);
+  thread myThread4(primes, 104, 2);
+  thread myThread5(primes, 105, 2);
+  thread myThread6(primes, 106, 2);
+  thread myThread7(primes, 107, 2);
   myThread.join();
   myThread1.join();
   myThread2.join();
@@ -61,10 +61,10 @@ while(1){
 bool is_prime(unsigned long int num)
 {
   // Only have to check for divisible for the sqrt(number)
-  int upper = sqrt(num);
+  long int upper = sqrt(num);
 
   // Check if the number is evenly divisible (start at 2 going up)
-  for (unsigned long int cnum = 2; cnum <= upper; cnum++)
+  for (long int cnum = 2; cnum <= upper; cnum++)
   {
     unsigned long int mod = num % cnum; // Remainder
 
